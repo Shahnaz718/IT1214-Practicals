2375f0230e284d1c0b2a36292170d67e4a864878
Shahnaz718 <shahnazaatham123@gmail.com>
2025-05-08 00:31:01 +0530

    Added Session01

---
+++ Session01/Args (1).java
@@ -0,0 +1,15 @@
+//this is a comment
+//this is our first java program
+
+import java.io.*; //this is how we import packages
+
+class MySecondClass
+	{ //class name should start with capital letter
+	public static void main(String[] ar) {
+		System.out.println(ar[0]+" "+ar[2]+" "+ar[1]);
+
+	}
+}
+
+//java MySecondClass How are you
+//output How you are
---
+++ Session01/MyfirstjavaProgram (1).java
@@ -0,0 +1,14 @@
+//this is a comment
+//this is our first java program
+
+import java.io.*; //this is how we import packages
+
+class MyFirstClass
+	{ //class name should start with capital letter
+	public static void main(String[] args) {
+		System.out.print("Hi");
+		System.out.print("This is IT1214 Practical session 1");
+		System.out.println("This prints next in a new line");
+		System.out.println("This is in a new line");
+	}
+}
